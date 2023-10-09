b39e8c89b5ad7fa8510e4b2ebfc4f2c1508df3ef
rauldangio <jojofagqwrt@gmail.com>
2023-10-09 14:59:37 -0300

    terminar de criar a pagina de login

--- stochos-front-R/src/pages/login/index.tsx
+++ stochos-front-R/src/pages/login/index.tsx
@@ -1,13 +1,42 @@
+import { Button } from "@mui/material";
 import style from "./Login.module.scss";
 
-export default function Login(){
+export default function Login() {
   return (
     <div className={style.container}>
       <div className={`${style.subcontainer}`}>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div>
+          <h1>STOCHOS</h1>
+        </div>
+        <div>
+          <p>Uma nova forma de organização</p>
+        </div>
+        <div>
+          <Button>botao</Button>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <h1>Login</h1>
+        </div>
+        <div>
+          <div>
+            <form action="">
+              <label>Email</label>
+              <input type="text" />
+              <label>Senha</label>
+              <input type="text" />
+            </form>
+          </div>
+          <div>
+            <p>esqueceu a senha?</p>
+          </div>
+        </div>
+        <div>
+          <Button>Login</Button>
+        </div>
       </div>
     </div>
-  )
+  );
 }
