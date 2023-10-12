7e1e8648aaee1371c7262e0705c1e3e94b058b0f
raul dangio <jojofagqwrt@gmail.com>
2023-10-12 09:22:05 -0300

    estilizar a pagina de meta

--- stochos-front-R/src/pages/metas/index.tsx
+++ stochos-front-R/src/pages/metas/index.tsx
@@ -1,15 +1,41 @@
 import Card from "../../components/Card";
 import dataMeta from "./meta.json";
+import dataUser from "../usuario/userinfo.json";
 
-export default function Metas(){
+export default function Metas() {
+    let userinfo = dataUser[0]
+    console.log(userinfo)
+    console.log(dataUser)
 
-    return(
+    return (
+        <div>
             <div>
-                {dataMeta.map((value => (
-                    <div>
-                        <Card type="meta" data={value.data} nome={value.nome} nomeremetente={value.nomeremetente} urgencia={value.urgencia}/>
-                    </div>
-                )))}
+                <h1>Metas Criadas</h1>
+                <div>
+                    {dataMeta.map((value => {
+                        if (value.nomeremetente === userinfo.nome) {
+                            return <Card type="meta" data={value.data} nome={value.nome} nomeremetente={value.nomeremetente} urgencia={value.urgencia} />
+                        }
+                        return null;
+                    }))}
+                </div>
             </div>
-        )
+
+            <div>
+                <h1>Metas Para Fazer</h1>
+                <div>
+                    {dataMeta.map((value => {
+                        if (value.nomeremetente !== userinfo.nome) {
+                            return <Card type="meta" data={value.data} nome={value.nome} nomeremetente={value.nomeremetente} urgencia={value.urgencia} />
+                        }
+                        return null;
+                    }))}
+                </div>
+            </div>
+
+            <div>
+                <button>Criar Meta</button>
+            </div>
+        </div>
+    )
 }
