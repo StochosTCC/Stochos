2908b259ddd8ea042b4706df4719225dab086c7d
raul dangio <jojofagqwrt@gmail.com>
2023-10-12 01:20:49 -0300

    card meta criado

--- stochos-front-R/src/components/Card/index.tsx
+++ stochos-front-R/src/components/Card/index.tsx
@@ -1,9 +1,68 @@
+import { Avatar } from "@mui/material";
 import style from "./Card.module.scss";
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import { deepOrange, deepPurple, pink, red } from "@mui/material/colors";
 
-export default function Card(){
+interface Props {
+    type: string
+    nome: string
+    nomeremetente: string,
+    urgencia: number,
+    data: string
+}
+
+
+
+export default function Card({ type, nome, nomeremetente, data, urgencia }: Props) {
+    let color = ""
+    if (type === "meta") {
+        if (urgencia === 1) {
+            color = style.urgencia1
+        }
+        else if (urgencia === 2) {
+            color = style.urgencia2
+        }
+        else if (urgencia === 3) {
+            color = style.urgencia3
+        }
+    }
     return (
-        <div>
-            card
+        <div className={style.card}>
+            <div className={style.cardcima}>
+                <div>
+                    <p className={style.nomemeta}>{nome}</p>
+                    <p className={style.nomeremetente}>{nomeremetente}</p>
+                </div>
+                <div>
+                    {randomColor(nomeremetente)}
+                </div>
+            </div>
+
+            <div className={style.cardbaixo}>
+                <SettingsOutlinedIcon className={style.icon} sx={{ fontSize: 40 }}></SettingsOutlinedIcon>
+                <div className={style.urg_e_data}>
+                    <div className={color}></div>
+                    <p>{data}</p>
+                </div>
+            </div>
         </div>
     )
+}
+
+
+function randomColor(nomeremetente: string) {
+    let random = Math.floor(Math.random() * 4)
+    let size = 46
+    let fontSize = 30
+    switch (random) {
+        case 1:
+            return <Avatar sx={{ bgcolor: deepPurple[500], width: size, height: size, fontSize:fontSize }}>{nomeremetente[0]}</Avatar>
+        case 2:
+            return <Avatar sx={{ bgcolor: deepOrange[500], width: size, height: size, fontSize:fontSize }}>{nomeremetente[0]}</Avatar>
+        case 3:
+            return <Avatar sx={{ bgcolor: pink[500], width: size, height: size, fontSize:fontSize }}>{nomeremetente[0]}</Avatar>
+        case 4:
+            return <Avatar sx={{ bgcolor: red[500], width: size, height: size, fontSize:fontSize }}>{nomeremetente[0]}</Avatar>
+    }
+    return <Avatar sx={{ bgcolor: red[500], width: size, height: size, fontSize:fontSize }}>{nomeremetente[0]}</Avatar>
 }
--- stochos-front-R/src/pages/metas/index.tsx
+++ stochos-front-R/src/pages/metas/index.tsx
@@ -1,6 +1,15 @@
+import Card from "../../components/Card";
+import dataMeta from "./meta.json";
+
 export default function Metas(){
 
     return(
-        <div>metas</div>
-    )
+            <div>
+                {dataMeta.map((value => (
+                    <div>
+                        <Card type="meta" data={value.data} nome={value.nome} nomeremetente={value.nomeremetente} urgencia={value.urgencia}/>
+                    </div>
+                )))}
+            </div>
+        )
 }
