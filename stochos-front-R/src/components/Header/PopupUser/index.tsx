cdbd4e8343fd4fb87ac8afa141f5640c042c50c1
Vini27Br <viniciusouza0@gmail.com>
2023-10-16 08:03:18 -0300

    0.1

---
+++ stochos-front-R/src/components/Header/PopupUser/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Popover from '@mui/material/Popover';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import HeaderPopover from '../HeaderPopover/index';
+
+
+export default function PopoverUser() {
+
+    return ( 
+        <div>
+
+        </div>
+    )}
--- stochos-front-R/src/components/Header/index.tsx
+++ stochos-front-R/src/components/Header/index.tsx
@@ -1,30 +1,45 @@
 import style from "./Header.module.scss";
 import logo from "../../assets/longlogo.png";
 import Popup from "./Popup";
-import { GrupoMeta } from '../../enums/GrupoMeta/GrupoMeta';
+import { GrupoMeta } from "../../enums/GrupoMeta/GrupoMeta";
+import { Avatar } from "@mui/material";
+import dataUser from "../../pages/usuario/userinfo.json"
+import React from "react";
 
+export default function Header() {
+  const [anchorEl, setAnchorEl] = React.useState<HTMLDivElement | null>(null);
 
-export default function Header(){
+  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
 
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-    return <>
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
+
+  return (
+    <>
       <header className={style.header}>
         <div className={style.nav}>
           <div className={style.logo}>
-          <img  className={style.logofoto} src={logo} alt="Logo"/>
+            <img className={style.logofoto} src={logo} alt="Logo" />
           </div>
 
           <div className={style.grupometa}>
-            <Popup type={GrupoMeta.GRUPO}/>
-            <Popup type={GrupoMeta.META}/>
-
+            <Popup type={GrupoMeta.GRUPO} />
+            <Popup type={GrupoMeta.META} />
           </div>
 
           <div className={style.usuarioinfo}>
-            <span className={style.nomedeusuario}>Nome de usuario</span>
-            <img className={style.fotodeperfil} src="" alt="Foto de Perfil" />
+            <span className={style.nomedeusuario}>{dataUser[0].nome}</span>
+            
+            <div><Avatar onClick={handleClick}>{dataUser[0].nome[0]}</Avatar></div>
           </div>
         </div>
       </header>
     </>
+  );
 }
