3a0bfa880b764127c04594ac62db0d96b9d8dc8c
Vini27Br <viniciusouza0@gmail.com>
2023-10-16 09:18:51 -0300

    finalizado popup user

--- stochos-front-R/src/components/Header/PopupUser/index.tsx
+++ stochos-front-R/src/components/Header/PopupUser/index.tsx
@@ -1,14 +1,27 @@
-import React from "react";
-import Popover from '@mui/material/Popover';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import HeaderPopover from '../HeaderPopover/index';
-
+import styles from "./PopupUser.module.scss"
+import Button from "@mui/material/Button";
 
 export default function PopoverUser() {
-
-    return ( 
-        <div>
-
-        </div>
-    )}
+  return (
+    <>
+    <div className={styles.botoes}>
+     
+        <a href="/Admin">
+            <Button className={styles.botao}>
+                Administrador</Button>
+        </a>
+     
+   
+     
+        <a href="/Usuario">
+            
+        <Button className={styles.botao}>Usuário</Button>
+        </a>
+     
+        <a href="/Login">
+        <Button className={styles.botao}>Sair</Button>
+        </a>
+    </div>
+    </>
+  );
+}
