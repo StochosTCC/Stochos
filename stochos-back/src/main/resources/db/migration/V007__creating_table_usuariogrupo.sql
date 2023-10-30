7b00dbe2a5afe64de7051e46229278d2814b1977
rauldangio <jojofagqwrt@gmail.com>
2023-10-30 11:38:22 -0300

    add if statement

--- stochos-back/src/main/resources/db/migration/V007__creating_table_usuariogrupo.sql
+++ stochos-back/src/main/resources/db/migration/V007__creating_table_usuariogrupo.sql
@@ -19,6 +19,7 @@ alter table usuario_cargo add constraint fk_usuario_cargo_cargo foreign key(idca
 
 
 insert into usuario_grupo values(0, 1, 1),(0, 1, 2);
+insert into usuario_grupo values(0, 2, 1),(0, 1, 3);
 insert into usuario_cargo values(0, 2, 1),(0, 2,2);
 insert into usuario_cargo values(0, 2, 3);
 
